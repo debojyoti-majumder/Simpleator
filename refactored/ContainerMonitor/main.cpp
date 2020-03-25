e122cffab92a214f7bba33e5ee04bea0f1a2cb0b
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-03-25 19:59:31 +0530

    Just the process creation method.

--- refactored/ContainerMonitor/main.cpp
+++ refactored/ContainerMonitor/main.cpp
@@ -1,6 +1,35 @@
-#include <tchar.h>
-#include <Windows.h>
+// MyVMProv.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-int _tmain( int argc TCHAR* argv[] ) {
-  return 0;
+#include "pch.h"
+
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+
+int wmain(int argc, wchar_t* argv[])
+{
+    STARTUPINFOEX emulatorStartupInformation;
+    PROCESS_INFORMATION emulatorProcessInfo;
+
+    // Intilizing start up information for the process
+    ZeroMemory(&emulatorProcessInfo, sizeof(emulatorProcessInfo));
+    ZeroMemory(&emulatorStartupInformation, sizeof(emulatorStartupInformation));
+
+    // Creating the process, in a suspended state
+    auto processCreatingResult = CreateProcess(L"emulator.exe",
+            GetCommandLine(), NULL, NULL, TRUE,
+            CREATE_SUSPENDED | EXTENDED_STARTUPINFO_PRESENT,
+            NULL, NULL,
+            &emulatorStartupInformation.StartupInfo,
+            &emulatorProcessInfo);
+
+    // Checking for error codes
+    if (false == processCreatingResult) {
+        std::wcout << "Was not able to launch the process\n";
+        return -1;
+    }
+
+    // Waiting for user input to continue
+    getch();
+    return 0;
 }
